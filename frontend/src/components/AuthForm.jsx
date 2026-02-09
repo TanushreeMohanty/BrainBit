af1de7ecedc2c0d408b5094221f2e8919cf2007c
TanushreeMohanty <122718553+TanushreeMohanty@users.noreply.github.com>
2026-02-09 20:30:56 +0530

    Login/Signup UI fixed

--- frontend/src/components/AuthForm.jsx
+++ frontend/src/components/AuthForm.jsx
@@ -9,7 +9,7 @@ const AuthForm = ({
   return (
     <div className="login-container">
       <div className="login-box">
-        <h2><img id='logo' src={img} alt="BrainBit Logo" />BrainBit {isSignupMode ? " Signup" : " Login"} </h2>
+        <h2><img id='logo' src={img} alt="BrainBit Logo" /> {isSignupMode ? " Signup" : " Login"} </h2>
         <form onSubmit={onSubmit}>
           <input 
             type="text" placeholder="Username" 
