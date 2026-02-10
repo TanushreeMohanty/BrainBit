df319d1f635d41c9e0aa1664ca135fa092b95205
TanushreeMohanty <122718553+TanushreeMohanty@users.noreply.github.com>
2026-02-11 00:53:23 +0530

    Navbar CSS fixed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import img from '../assets/logo.png';
-
+import './Navbar.css';
 const Navbar = ({ user, onLogout, onNavigate }) => {
   return (
     <nav className="navbar">
